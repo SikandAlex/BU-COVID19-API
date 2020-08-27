01e81669890f3b4190f09ab8a8088568f72c849e
Alex Sikand <alex@sikand.org>
2020-08-27 18:18:51 -0400

    Remove all code referencing pandas and twilio

--- data_fetcher.py
+++ data_fetcher.py
@@ -15,33 +15,6 @@ from chromedriver_py import binary_path # this will get you the path variable
 
 import requests
 
-def send_sms_notification(to, message_body):
-    client = Client(account_sid, auth_token)
-    client.messages.create(to=to,
-                           from_=twilio_number,
-                           body=message_body)
-
-def sendMessages():
-    response = requests.get('https://docs.google.com/spreadsheets/d/1oRcFPlYSlevPyGm_8xeC4S_ZOtkux_wk96aVF-9hOd8/export?format=csv&gid=1622819468')
-    f = open("contactSheet.csv","w")#write mode
-    f.write(response.content.decode('ASCII'))
-    f.close()
-
-    df = pandas.read_csv("contactSheet.csv")
-    contactList = df["What is your phone number?"].to_numpy()
-    df = None
-
-    r = requests.get(url="https://bu-covid19-api.herokuapp.com/current")
-    data = r.json()
-    key1 = list(data.keys())[0]
-    positive = data[key1]["Daily"]["Positive"]
-    isolated = data[key1]["Daily"]["Isolated"]
-
-    msg = "\nBU COVID-19 Tracker\n" + str(positive) + " new positive cases today at BU"
-
-    for c in contactList:
-        send_sms_notification(c, msg)
-
 
 
 chrome_options = Options()
