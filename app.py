fd8a60ca1cc13112cb80c91f298b0b44dbcb8f3f
Alex Sikand <alex@sikand.org>
2020-08-31 22:50:36 -0400

    Add Flask CORS

--- app.py
+++ app.py
@@ -15,12 +15,21 @@ from selenium.common.exceptions import TimeoutException
 # Custom function to grab new data from Healthway dashboard
 from data_fetcher import update_data, get_ngx, get_ngx_all
 
+
+from flask_cors import CORS, cross_origin
+
+
+
+
+
 update_data()
 
 # Flask Config
 app = Flask(__name__)
 api = Api(app)
+cors = CORS(app)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 # Schedule the data update
 sched = BackgroundScheduler(daemon=True)
@@ -28,26 +37,31 @@ sched.add_job(update_data,'interval',minutes=15)
 sched.start()
 
 @app.route("/all", methods=['GET'])
+@cross_origin()
 def all():
     with open('data.json') as json_file:
         jsonObj = json.load(json_file)
         return jsonObj
 
 @app.route("/current", methods=['GET'])
+@cross_origin()
 def current():
     with open('data.json') as json_file:
         jsonObj = json.load(json_file)
         return jsonObj["data"][-1]
 
 @app.route("/ngx/<string:metric>", methods=['GET'])
+@cross_origin()
 def ngx(metric):
     return get_ngx(metric=metric)
 
 @app.route("/ngx-all", methods=['GET'])
+@cross_origin()
 def ngx_all():
     return get_ngx_all()
 
 @app.route("/ngx-infections", methods=['GET'])
+@cross_origin()
 def ngx_infections():
     with open('data.json') as json_file:
         jsonObj = json.load(json_file)
