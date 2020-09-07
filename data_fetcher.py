28723f7b64a6d90872718e9cabc6cde6fb0e2887
Alex Sikand <alex@sikand.org>
2020-09-06 22:40:43 -0400

    Fix to correct after BU changed HTML structure

--- data_fetcher.py
+++ data_fetcher.py
@@ -40,57 +40,49 @@ def update_data():
     daily_tests_conducted_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[8]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[1]/div[1]'
     daily_tests_pos_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[8]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[3]/div[1]'
     daily_tests_neg_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[8]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[2]/div[1]'
-    daily_tests_inv_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[8]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[4]/div[1]'
-    avg_sample_process_time_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[17]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
+    avg_sample_process_time_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[14]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
 
     daily_tests_conducted = driver.find_element(By.XPATH, daily_tests_conducted_xpath).text
     daily_tests_neg = driver.find_element(By.XPATH, daily_tests_neg_xpath).text
     daily_tests_pos = driver.find_element(By.XPATH, daily_tests_pos_xpath).text
-    daily_tests_inv = driver.find_element(By.XPATH, daily_tests_inv_xpath).text
     avg_sample_process_time = driver.find_element(By.XPATH, avg_sample_process_time_xpath).text
 
     # CUMULATIVE DATA
-    cum_title_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[14]/transform/div/div[3]/div/visual-modern/div/svg/g[1]/text/tspan'
-    cum_tests_conducted_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[20]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[1]/div[1]'
-    cum_tests_neg_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[20]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[2]/div[1]'
-    cum_tests_inv_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[20]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[4]/div[1]'
-    cum_tests_pos_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[20]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[3]/div[1]'
-    cum_avg_sample_process_time_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[18]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
+    #cum_title_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[14]/transform/div/div[3]/div/visual-modern/div/svg/g[1]/text/tspan'
+    cum_tests_conducted_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[17]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[1]/div[1]'
+    cum_tests_neg_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[17]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[2]/div[1]'
+    cum_tests_pos_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[17]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div[3]/div[1]'
+    cum_avg_sample_process_time_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[15]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
 
     cum_tests_conducted = driver.find_element(By.XPATH, cum_tests_conducted_xpath).text
     cum_tests_neg = driver.find_element(By.XPATH, cum_tests_neg_xpath).text
     cum_tests_pos = driver.find_element(By.XPATH, cum_tests_pos_xpath).text
-    cum_tests_inv = driver.find_element(By.XPATH, cum_tests_inv_xpath).text
     cum_avg_sample_process_time = driver.find_element(By.XPATH, cum_avg_sample_process_time_xpath).text
 
     # infected student info
-    recovered_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[24]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
-    noncontagious_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[16]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
+    recovered_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[21]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
     isolated_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[9]/transform/div/div[3]/div/visual-modern/div/div/div/div[1]/div/div/div/div/div/div[1]'
 
     recovered = driver.find_element(By.XPATH, recovered_xpath).text
-    noncontagious = driver.find_element(By.XPATH, noncontagious_xpath).text
     isolated = driver.find_element(By.XPATH, isolated_xpath).text
 
+    verbose_date_xpath = '//*[@id="pvExplorationHost"]/div/div/exploration/div/explore-canvas-modern/div/div[2]/div/div[2]/div[2]/visual-container-repeat/visual-container-modern[11]/transform/div/div[3]/div/visual-modern/div/svg'
     # Nicely formatted date for daily view
     date_data = driver.find_elements(By.CLASS_NAME, 'card')
-    verbose_date = date_data[9].get_attribute("aria-label")
+    verbose_date = date_data[8].get_attribute('aria-label')
     verbose_date = verbose_date.replace("Latest ResultLoadDate ", '')
     verbose_date = verbose_date.replace(".", '')
 
     daily_tests_conducted = int(daily_tests_conducted.replace(",", ''))
     daily_tests_neg = int(daily_tests_neg.replace(",", ''))
     daily_tests_pos = int(daily_tests_pos.replace(",", ''))
-    daily_tests_inv = int(daily_tests_inv.replace(",", ''))
 
     isolated = int(isolated.replace(",", ''))
     recovered = int(recovered.replace(",", ''))
-    noncontagious = int(noncontagious.replace(",", ''))
 
     cum_tests_conducted = int(cum_tests_conducted.replace(",", ''))
     cum_tests_neg = int(cum_tests_neg.replace(",", ''))
     cum_tests_pos = int(cum_tests_pos.replace(",", ''))
-    cum_tests_inv = int(cum_tests_inv.replace(",", ''))
 
     avg_sample_process_time = float(avg_sample_process_time)
     cum_avg_sample_process_time = float(cum_avg_sample_process_time)
@@ -103,21 +95,20 @@ def update_data():
                     'Total': daily_tests_conducted,
                     'Negative': daily_tests_neg,
                     'Positive': daily_tests_pos,
-                    'Invalid': daily_tests_inv,
                     'Isolated': isolated,
                     'Recovered': recovered,
-                    'Non-Contagious': noncontagious,
                     'Processing Time': avg_sample_process_time
                     },
                 'Cumulative': {
                     'Total': cum_tests_conducted,
                     'Negative': cum_tests_neg,
                     'Positive': cum_tests_pos,
-                    'Invalid': cum_tests_inv,
                     'Processing Time': cum_avg_sample_process_time
                     }
                 }
             }
+
+    print(result)
 
     driver.quit()
 
